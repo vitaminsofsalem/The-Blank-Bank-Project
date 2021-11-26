ec4d48fc5e0b3047ff545dc7be7a6bed480b7576
John Ehab <82682854+John-Ghaly88@users.noreply.github.com>
2021-11-26 18:33:50 +0200

    Added account schema for the dashboard

--- Server/src/schemas/account.schema.ts
+++ Server/src/schemas/account.schema.ts
@@ -1,22 +1,18 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document, Mongoose, ObjectId, SchemaTypes, Types } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes, ObjectId, Types } from "mongoose";
 
 export type AccountDocument = Account & Document;
 
 @Schema()
 export class Account {
     @Prop({ type: SchemaTypes.ObjectId, ref: 'User' , required: true })
-    userId: Types.ObjectId;
+    userID: Types.ObjectId;
 
     @Prop({ required: true })
-    openDate: Date;
+    accountNo: Number;
 
     @Prop({ required: true })
-    type: string;
-
-    @Prop({ required: true })
-    balance: number;
-
+    balance: Number;
 }
 
 export const AccountSchema = SchemaFactory.createForClass(Account);
