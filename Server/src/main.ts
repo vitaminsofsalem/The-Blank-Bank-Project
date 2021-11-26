b6b4281b0a49a420831e444adccbc0fbf8329fb9
Youssef Henna <youssef.hisham14@gmail.com>
2021-11-26 11:11:09 +0200

    Added register communication with api

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: "http://localhost:3000" });
   await app.listen(process.env.PORT);
 }
 bootstrap();
